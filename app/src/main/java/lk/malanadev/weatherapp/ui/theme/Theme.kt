6364253a4497e0c961a246abaa8063ba2d9c8db7
malandev <malandev95@gmail.com>
2025-01-14 09:10:39 +0530

    Initialize project

---
+++ app/src/main/java/lk/malanadev/weatherapp/ui/theme/Theme.kt
@@ -0,0 +1,30 @@
+package lk.malanadev.weatherapp.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+private val AppColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
+
+
+@Composable
+fun WeatherAppTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = AppColorScheme,
+        typography = Typography,
+        content = content
+    )
+}
